9bf2ba41227cf5b3f1cbee80f513b0766100b411
albertoconcovia <concovia@gmail.com>
2021-12-02 14:45:37 +0000

    Home.js text updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,3 +1,11 @@
-const Home = () => <div>Home!</div>;
+const Home = () => 
+
+    <div>
+        <p> Welcome to the home of the Justice Department of Fakelandia.</p>
+        <p>
+            Here you can browse a list of recent misdemeanour committed by our citizens, or you can confess to your own misdemeanour.
+        </p>
+
+    </div>;
 
 export default Home;
