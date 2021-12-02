25c1c7dfada0cabe81d9bad2674df6535409a9e9
albertoconcovia <concovia@gmail.com>
2021-12-02 21:13:45 +0000

    activeClassName style added into Nav.js

--- src/components/header/Nav.js
+++ src/components/header/Nav.js
@@ -1,13 +1,14 @@
-import { NavLink }  from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 
 const Nav = () => (
   <nav >
-    <ul className="wrapper">
-      <li className="nav-links"><NavLink to='/'>Home</NavLink></li>
-      <li className="nav-links"><NavLink to='/Confession'>Confession</NavLink></li>
-      <li className="nav-links"><NavLink to='/Misdemeanour'>Misdemeanour</NavLink></li>
+    <ul className="navbar-nav" >
+      <li className="nav-item ">FAKELANDIA JUSTICE DEPARTMENT</li>
+      <li className="nav-item "> <NavLink activeClassName="active" to="/" > Home </NavLink>   </li>
+      <li className="nav-item "> <NavLink activeClassName="active" to="/Confession" >Confess To Us</NavLink>  </li>
+      <li className="nav-item "> <NavLink activeClassName="active" to="/Misdemeanour"> Misdemeanour </NavLink>  </li>
     </ul>
   </nav>
 );
-
+    
 export default Nav;
