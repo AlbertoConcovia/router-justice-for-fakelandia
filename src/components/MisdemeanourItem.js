87e63ee24b885c21978f24e3861f541afe852808
albertoconcovia <concovia@gmail.com>
2021-12-03 00:28:28 +0000

    picsum added in Misdemeanour.js

--- src/components/MisdemeanourItem.js
+++ src/components/MisdemeanourItem.js
@@ -1,26 +1,27 @@
-import React, { useContext } from 'react';
-import { FavouritesContext } from './pages/Misdemeanour';
+import React, { useContext } from "react";
+import { FavouritesContext } from "./pages/Misdemeanour";
 
 function MisdemeanourItem() {
+  const { misdemeanour, setMisdemeanour } = useContext(FavouritesContext);
 
-const { misdemeanour, setMisdemeanour } = useContext(FavouritesContext);
-
-return (
-  <div className="row">
-    <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-      <div className="box">{misdemeanour[0]?.citizenId}</div>
+  return (
+    <div className="row">
+      <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+        <div className="box">{misdemeanour[0]?.citizenId}</div>
+      </div>
+      <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+        <div className="box">{misdemeanour[0]?.date}</div>
+      </div>
+      <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+        <div className="box">{misdemeanour[0]?.misdemeanour}</div>
+      </div>
+      <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+        <div className="box">
+          <img src="https://picsum.photos/100/40" />
+        </div>
+      </div>
     </div>
-    <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-      <div className="box">{misdemeanour[0]?.date}</div>
-    </div>
-    <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-      <div className="box">{misdemeanour[0]?.misdemeanour}</div>
-    </div>
-    <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-      <div className="box">Punishment Idea 1</div>
-    </div>
-  </div>
-);
+  );
 }
 
 export default MisdemeanourItem;
